761f8dc1098ef7b790cc55210b3d94425bab8d2d
marial-ko <lysenko-maria75@mail.ru>
2021-01-12 19:46:17 +0500

    Добавлено 4 задание

---
+++ PhotoEnhancer/PhotoEnhancer/Filters/Parameters/IParametersFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public interface IParametersFactory<TParameters>
+    {
+        ParameterInfo[] GetDescription();
+        TParameters CreateParameters(double[] values);
+    }
+}
